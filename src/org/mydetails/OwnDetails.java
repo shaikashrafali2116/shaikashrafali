889bf9005eac01dd96c8925de9b98405c6c36369
shaikashrafali <shaikashrafali2116@gmail.com>
2023-01-23 16:46:41 +0530

    GitAshraf

---
+++ src/org/mydetails/OwnDetails.java
@@ -0,0 +1,34 @@
+package org.mydetails;
+
+public class OwnDetails {
+
+	public static void main(String[] args) {
+
+		byte age = 27;
+		System.out.println("My age :" + age);
+
+		short atmPin = 1234;
+		System.out.println("ATM pin :" + atmPin);
+
+		int phnum = 994435699;
+		System.out.println("My Number is : " + phnum);
+
+		long num = 9994540186l;
+		System.out.println("Dad number is :" + num);
+
+		float dec = 23.5465165468465f;
+		System.out.println("Decimal is :" + dec);
+
+		double avg = 3.141351315465;
+		System.out.println("Avergage :" + avg);
+
+		char gender = 'M';
+		System.out.println("My gender is :" + gender);
+
+		boolean decision = true;
+		System.out.println("Decision is :" + decision);
+
+		String email = "shaikashraf0709@gmail.com";
+		System.out.println("Email is :" + email);
+	}
+}
